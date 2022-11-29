9b03b8178d861b3258a2c673d95b4f4a4950a3b8
jiac <jia_chao23@126.com>
2022-11-29 18:59:39 +0800

    first commit

---
+++ src/main/java/com/jcohy/yilin/YiLinApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiLinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiLinApplication.class, args);
+	}
+
+}
