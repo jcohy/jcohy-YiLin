9d05bf2e84a2e69de942dd466eecfe145922603a
Jcohy <jia_chao23@126.com>
2022-11-30 06:34:12 +0000

    first commit

--- src/main/java/com/jcohy/yilin/YiLinApplication.java
+++ src/main/java/com/jcohy/yilin/YiLinApplication.java
@@ -1,13 +1,22 @@
-package com.example.demo;
+package com.jcohy.yilin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class YiLinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YiLinApplication.class, args);
+	}
+
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
 	}
 
 }
